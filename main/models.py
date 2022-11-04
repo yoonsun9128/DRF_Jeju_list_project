f9645c757b1c56e5753e59f56a76875698810cc4
yoonsun9128 <yoonsun9128@gmail.com>
2022-11-04 11:26:18 +0900

    MOD main model change

--- main/models.py
+++ main/models.py
@@ -5,9 +5,18 @@ class Store(models.Model):
     store_id = models.CharField(max_length=100) # https://map.kakao.com/여기
     store_name = models.TextField() #상호명
     address = models.TextField() # 가게 주소
-    menu = models.TextField() # 가게 메뉴 카테고리 ex 한식 횟집
     star = models.TextField() # 가게 별점
-    content = models.TextField(null=True) #리뷰 내용
-    img = models.ImageField(null=True)
-
+    img = models.TextField(max_length=256, default='')
+    class meta:
+        db_table = 'store'
+class Tags(models.Model):
+    class meta:
+        db_table = 'tags'
+    name = models.CharField(max_length=256)
+class Reviews(models.Model):
+    class meta:
+        db_table = 'review'
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    content = models.TextField()
+    tag = models.ManyToManyField(Tags, related_name='review')
 
