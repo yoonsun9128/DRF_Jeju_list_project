4193bf9790c271fe9dd21116d642d932ab2873fb
OceanGim <rladlsghrk@gmail.com>
2022-11-05 17:43:52 +0900

    Add Reviews model, ProfileView

--- user/views.py
+++ user/views.py
@@ -1,14 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework import status, permissions
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from user.serealizers import CustomTokenObtainPairSerializer
-
-from user.serealizers import UserSerializer
+from user.serealizers import CustomTokenObtainPairSerializer, UserSerializer, UserProfileSerializer
+from user.models import User
 class UserView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
@@ -33,3 +33,8 @@ class mockView(APIView):
 
         return Response('get 요청')
     
+class ProfileView(APIView):
+    def get(self, request, user_id):
+        user= get_object_or_404(User, id=user_id)
+        serializer = UserProfileSerializer(user)
+        return Response(serializer.data)
