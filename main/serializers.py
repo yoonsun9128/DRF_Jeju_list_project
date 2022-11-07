2def906963c86b4c3dcb9bcc7f57c00e05ef57f4
nhkmi1001 <nhkmi1001@gmail.com>
2022-11-08 02:53:32 +0900

    [Mod] Backend finish

--- main/serializers.py
+++ main/serializers.py
@@ -2,33 +2,27 @@ from rest_framework import serializers
 from main.models import Store, Comment
 
 
-class StoreListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Store
-        fields = "__all__"
-
-
 class CommentSerializer(serializers.ModelSerializer):
+
     user = serializers.SerializerMethodField()
+    created_at = serializers.SerializerMethodField()
 
     def get_user(self, obj):
         return obj.user.username
 
+    def get_created_at(self, obj):
+        created_at = obj.created_at.replace(microsecond=0).isoformat()
+        return created_at
     class Meta:
         model = Comment
-        exclude = ("store", )
+        exclude = ("store",)
 
-
-class StoreSerializer(serializers.ModelSerializer):
-    user = serializers.SerializerMethodField()
-    comment_set = CommentSerializer(many=True)
+class StoreListSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True, source="comment_set")
 
     class Meta:
         model = Store
         fields = "__all__"
-
-
-
 
 
 class CommentCreateSerializer(serializers.ModelSerializer):
--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('', views.StoreListView.as_view(), name='storelist_view'), # 메인페이지에 처음 별점 높은 가게 순으로 보여줌.
     path('<int:store_id>/', views.StoreSearchView.as_view(), name='storeSearch_view'), # 검색창에 검색했을 때 검색한 가게만 보여줌
     path('<int:store_id>/comment/', views.CommentView.as_view(), name='comment_view'),
-    # path('<int:store_id>/comment/<int:comment_id>/', views.CommentDetailView.as_view(), name='comment_detail_view'),
 ]
